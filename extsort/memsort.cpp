0a24914e54fb6c11e1fa7221267c6a105c73ca20
Sascha Riemer <sascha.riemer@gmail.com>
2014-04-14 22:20:09 +0200

    Add in-memory sort

--- extsort/memsort.cpp
+++ extsort/memsort.cpp
@@ -4,11 +4,41 @@
 #include <algorithm>
 #include <cstdint>
 #include <cstdlib>
+#include <iostream>
 
 #include <sys/mman.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+using namespace std;
 
 void memSort (int fd, uint64_t size) {
     uint64_t* buffer = static_cast<uint64_t*>(mmap(NULL, size * sizeof(uint64_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
     std::sort(buffer, buffer+size);
     munmap(buffer, size * sizeof(uint64_t));
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        cout << "Usage:" << endl;
+        cout << argv[0] << " <inputFile> <outputFile>" << endl << endl;
+        exit (1);
+    }
+
+    struct stat fileInfo;
+    stat(argv[1], &fileInfo);
+    uint64_t numElements = fileInfo.st_size / sizeof(uint64_t);
+
+    int fdInput = open (argv[1], O_RDONLY);
+    int fdOutput = open (argv[2], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    sendfile(fdOutput, fdInput, 0, fileInfo.st_size);
+    close (fdInput);
+
+    memSort (fdOutput, numElements);
+    close (fdOutput);
+
+    return 0;
 }
