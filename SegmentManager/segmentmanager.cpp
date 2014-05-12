d30a7fef7de64d5ca84d478a626affb86b33e1b8
Sascha Riemer <sascha.riemer@gmail.com>
2014-05-13 00:16:14 +0200

    Get-Only implementation for SegmentManager

--- SegmentManager/segmentmanager.cpp
+++ SegmentManager/segmentmanager.cpp
@@ -1,4 +1,44 @@
 #include "segmentmanager.h"
 
-SegmentManager::SegmentManager() {
+#include <fstream>
+
+using std::fstream;
+
+SegmentManager::SegmentManager(BufferManager& bufman)
+    : bm(bufman) {
+    fstream input("segment0", ios::in | ios::binary);
+    bool parseOk = schema.ParseFromIstream(&input);
+    input.close();
+    if (!parseOk) {
+        schema.Clear();
+        writeSchema();
+    }
+}
+
+bool SegmentManager::createSegment(const char* relationName) {
+    // TODO: not implemented
+    //schema::Relation* r = s.add_relations();
+    //r->set_name(relationName);
+    //r->set_segment_id();
+    //r->set_sizeinpages(0);
+    return false;
+}
+
+SPSegment SegmentManager::getSegment(const char* relationName) const {
+    for (int64_t i = 0; i < schema.relations_size(); i++) {
+        if ( schema.relations(i).name().compare(relationName) == 0 ) {
+            uint64_t segmentId = schema.relations(i).segment_id();
+            uint64_t pageCount = schema.relations(i).sizeinpages();
+            return SPSegment(bm, segmentId, pageCount);
+        }
+    }
+    // nothing found
+    // TODO: create real exception
+    throw 0;
+}
+
+void SegmentManager::writeSchema() {
+    fstream output("segment0", ios::out | ios::binary);
+    schema.SerializeToOstream(&output);
+    output.close();
 }
--- SegmentManager/segmentmanager.h
+++ SegmentManager/segmentmanager.h
@@ -1,14 +1,21 @@
 #ifndef SEGMENTMANAGER_H
 #define SEGMENTMANAGER_H
 
-typedef int SPSegment;
+#include "buffermanager.h"
+#include "spsegment.h"
+
+#include "schema.pb.h"
 
 class SegmentManager
 {
     public:
-        SegmentManager();
+        SegmentManager(BufferManager& bufman);
         bool createSegment(const char* relationName);
-        SPSegment& getSegment(const char* relationName);
+        SPSegment getSegment(const char* relationName) const;
+    private:
+        void writeSchema();
+        BufferManager& bm;
+        schema::Schema schema;
 };
 
 #endif // SEGMENTMANAGER_H
